87b87da5e04d3774570806ffda37e1209c761a9a
Mark Tracy <tracy454@att.net>
2014-07-04 15:49:15 -0700

    Initial Commit

---
+++ GrocerySync-Swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GrocerySync-Swift
+//
+//  Created by Mark Tracy on 7/4/14.
+//  Copyright (c) 2014 Mark Tracy Special Engineering. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
